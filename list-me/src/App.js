a994631e5c6ac4eee957daf2e1bc434ec2a4aaac
IbrahimKuz <ibrahimkuziez@gmail.com>
2024-11-13 17:59:19 -0600

    Minor Changes to Sidebar

--- list-me/src/App.js
+++ list-me/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import { useState } from 'react';
 import Header from './header.js';
-import Modal from './Modal.js';
+import Sidebar from './sidebar.js';
 import './App.css';
 
 function App() {
-  
   return (
     <>
     <Header />
       <div className="App">
+        <Sidebar />
       </div>
       </>
   );
---
+++ list-me/src/sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './sidebar.css';
+
+function Header() {
+
+    return (
+    <div className='sidebar'>
+        <p>Posts</p>
+        <p>Calender</p>
+        <p>Analytics</p>
+        <p>Linked Accounts</p>
+        <p>AI Tools</p>
+        <p>Settings</p>
+    </div>
+    );
+}
+
+export default Header;
