da5d64cbfeb38d6c5ff57acc7b9d81144d3603c3
IbrahimKuz <ibrahimkuziez@gmail.com>
2024-11-20 18:09:54 -0600

    Commit

--- list-me/src/App.js
+++ list-me/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from './header.js';
 import Sidebar from './sidebar.js';
 import './App.css';
+import Posts from './posts.js';
 
 function App() {
     const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -14,6 +15,7 @@ function App() {
     <Header toggleSidebar={toggleSidebar}/>
       <div className="app">
         <Sidebar isOpen={isSidebarOpen} />
+        <Posts />
       </div>
       </>
   );
